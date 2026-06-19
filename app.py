a35ff5bcfaf5acff9262db28dcd985b129afc9d9
madara-yoo <bdalmalkqdash5@gmail.com>
2026-06-19 14:53:33 +0100

    updn65

--- app.py
+++ app.py
@@ -1,36 +1,41 @@
 import asyncio
 import os
 import pty
-import shlex
-import struct
-import fcntl
-import termios
+import subprocess
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import FileResponse
 
 app = FastAPI()
 
-# تشغيل الباش كعملية ترمينال
+# إعداد الـ PTY
 master, slave = pty.openpty()
+
+# تشغيل الـ Bash في الخلفية
+subprocess.Popen(
+    ['/bin/bash', '-i'],
+    preexec_fn=os.setsid,
+    stdin=slave,
+    stdout=slave,
+    stderr=slave,
+    env={**os.environ, 'TERM': 'xterm'}
+)
 
 async def terminal_reader(websocket: WebSocket):
     while True:
         try:
-            # قراءة المخرجات من الـ PTY
-            loop = asyncio.get_event_loop()
-            data = await loop.run_in_executor(None, os.read, master, 1024)
+            # قراءة المخرجات من الترمينال
+            data = os.read(master, 1024)
             if data:
                 await websocket.send_text(data.decode('utf-8', 'ignore'))
+            await asyncio.sleep(0.01) # تقليل استهلاك المعالج
         except Exception:
             break
 
 @app.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
     await websocket.accept()
-    # تشغيل عملية القراءة في الخلفية
     reader_task = asyncio.create_task(terminal_reader(websocket))
-    
     try:
         while True:
             data = await websocket.receive_text()
@@ -38,7 +43,7 @@ async def websocket_endpoint(websocket: WebSocket):
     except WebSocketDisconnect:
         reader_task.cancel()
 
-# تقديم الواجهة
+# تقديم الملفات
 app.mount("/webos", StaticFiles(directory="webos"), name="webos")
 
 @app.get("/")
