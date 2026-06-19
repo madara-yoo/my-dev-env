e511281b1d3ee4c6e5d957d9d556bacdfe76abdb
madara-yoo <bdalmalkqdash5@gmail.com>
2026-06-19 06:26:06 +0100

    updt09

--- app.py
+++ app.py
@@ -1,38 +1,43 @@
-from flask import Flask, send_from_directory
-from flask_socketio import SocketIO
+import os
 import pty
 import os
 import select
-import termios
-import struct
-import fcntl
 import subprocess
+from flask import Flask, send_from_directory
+from flask_socketio import SocketIO
 
 app = Flask(__name__, static_folder='.hidden_os')
-socketio = SocketIO(app)
+# إعداد SocketIO ليعمل مع Eventlet
+socketio = SocketIO(app, async_mode='eventlet')
 
+# مسار مجلد الملفات الثابتة
 @app.route('/')
 def index():
     return send_from_directory('.hidden_os', 'index.html')
 
+# التعامل مع مدخلات الترمينال
 @socketio.on('input')
 def handle_input(data):
-    # إرسال المدخلات من المتصفح إلى عملية الـ Terminal
-    os.write(master_fd, data.encode())
+    if master_fd:
+        os.write(master_fd, data.encode())
 
 def read_and_forward():
     while True:
-        # قراءة المخرجات من الترمينال وإرسالها للمتصفح
-        fd_list = [master_fd]
-        readable, _, _ = select.select(fd_list, [], [])
-        if master_fd in readable:
+        # مراقبة مخرجات الترمينال
+        r, _, _ = select.select([master_fd], [], [])
+        if master_fd in r:
             data = os.read(master_fd, 1024)
             socketio.emit('output', data.decode('utf-8', 'ignore'))
 
-# إنشاء جلسة PTY حقيقية
+# إعداد الجلسة الحقيقية (PTY)
 master_fd, slave_fd = pty.openpty()
+# تشغيل Bash داخل جلسة تيرمينال حقيقية
 subprocess.Popen(['/bin/bash'], stdin=slave_fd, stdout=slave_fd, stderr=slave_fd, preexec_fn=os.setsid)
 
 if __name__ == '__main__':
+    # البدء بالخلفية
     socketio.start_background_task(read_and_forward)
-    socketio.run(app, host='0.0.0.0', port=8080)
+    # الحصول على البورت من Render أو استخدام 8080 افتراضياً
+    port = int(os.environ.get("PORT", 8080))
+    # التشغيل باستخدام eventlet للثبات
+    socketio.run(app, host='0.0.0.0', port=port)
