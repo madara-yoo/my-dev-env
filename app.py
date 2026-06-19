701a085de6cb178a2eab116ad98ecf23bb95a209
madara-yoo <bdalmalkqdash5@gmail.com>
2026-06-19 06:56:16 +0100

    updt15

--- app.py
+++ app.py
@@ -1,9 +1,16 @@
 import os
-from flask import Flask, send_from_directory, request, jsonify
+import pty
+import select
+import subprocess
+from flask import Flask, send_from_directory
 from flask_socketio import SocketIO
 
 app = Flask(__name__, static_folder='webos')
 socketio = SocketIO(app, async_mode='eventlet')
+
+# إعداد الـ Terminal
+master_fd, slave_fd = pty.openpty()
+subprocess.Popen(['/bin/bash'], stdin=slave_fd, stdout=slave_fd, stderr=slave_fd, preexec_fn=os.setsid)
 
 @app.route('/')
 def index():
@@ -11,8 +18,15 @@ def index():
 
 @socketio.on('input')
 def handle_input(data):
-    # هنا يتم استقبال الأوامر من المتصفح
-    print(f"Received: {data}")
+    os.write(master_fd, data.encode())
+
+def read_and_forward():
+    while True:
+        r, _, _ = select.select([master_fd], [], [])
+        if master_fd in r:
+            data = os.read(master_fd, 1024)
+            socketio.emit('output', data.decode('utf-8', 'ignore'))
 
 if __name__ == '__main__':
+    socketio.start_background_task(read_and_forward)
     socketio.run(app, host='0.0.0.0', port=10000)
